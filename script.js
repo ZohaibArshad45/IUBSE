bdd001e2cd336b6fa16d63d33ec9b0a33f44c6b5
Zohaib Arshad <109464646+ZohaibArshad45@users.noreply.github.com>
2023-05-10 08:07:22 +0500

    ProjectFinal5th-Semester

---
+++ script.js
@@ -0,0 +1,8 @@
+$('#menu-btn').click (function(){
+    $('nav .navigation ul').addClass('active')
+
+});
+$('#menu-close').click (function(){
+    $('nav .navigation ul').removeClass('active')
+
+});
